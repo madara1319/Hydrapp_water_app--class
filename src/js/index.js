8324ef43b6ad065e879b1b4f3086b8654cbaa31d
madara1319 <mateusz.bik@op.pl>
2022-10-13 19:19:36 +0200

    Some js updates

--- src/js/index.js
+++ src/js/index.js
@@ -5,25 +5,65 @@ import '../scss/main.scss';
 // registerSW();
 
 /* place your code below */
+
 const add=document.querySelector('.add');
 const substract=document.querySelector('.substract');
 var i=0;
-add.addEventListener("click",()=>{
-	i++;
-	console.log(i);	
-})
 
-substract.addEventListener("click",()=>{
-	i--;
+if (i>=0){
+
+	substract.addEventListener("click",()=>{
+		i-=1;
+		console.log(i);
+		if (i<0) {
+			i=0;
+			console.log(i);
+		}
+	})
+
+	add.addEventListener("click",()=>{
+		i+=1;
+		console.log(i);	
+		if (i<0) {
+			i=0;
+			console.log(i);
+		}
+	})
+}
+else
+{
+	i=0;
 	console.log(i);
-})
 
+}
 
+//if (i<=0){
+//	
+//}
 
 
 const Click = ()=> {
+
+
 	
 	const counter=document.querySelector(".counter--js"); 
 	counter.innerHTML=`${i}`;
 }
+
+
+
+	
+//const counter=document.querySelector(".counter--js");
+//counter.innerHTML=`${i}`;
+
 add.addEventListener("click",Click);
+substract.addEventListener("click",Click);
+
+const currentValue=localStorage.getItem('counter--js');
+let result='';
+if (currentValue){
+	console.log(`Liczba spozytych szklanek wody wynosi ${currentVlaue}`);
+	result=currentValue;
+}
+
+
